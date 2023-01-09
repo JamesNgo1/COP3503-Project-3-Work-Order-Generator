2dda5df13ff28ad55cdd4c3b49da661739dd937c
James Ngo <Jamestngo02@gmail.com>
2023-01-08 19:23:06 -0500

    Delete Project III n01471175/src directory

--- Project III n01471175/src/DataHandler.java
+++
@@ -1,9 +0,0 @@
-//data handler interface
-public abstract interface DataHandler 
-{
-	// getInfo and getFIle data 
-	public abstract String getInfo();
-	
-	public abstract String getFileData();
-
-}
